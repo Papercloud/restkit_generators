650a85411ebf0871fda2f6a323e941bd090acb39
Tomas Spacek <tomspacek@gmail.com>
2014-01-10 13:58:09 +1100

    Basic Core Data schema generation in

--- lib/generators/restkit_generators/model/model_generator.rb
+++ lib/generators/restkit_generators/model/model_generator.rb
@@ -14,7 +14,38 @@ module RestkitGenerators
       append_file "gen/Generated.h", "#include \"#{overwrite_filename}.h\""
     end
 
+    def generate_core_data_schema
+      empty_directory "gen/DataModel.xcdatamodeld"
+      empty_directory "gen/DataModel.xcdatamodeld/DataModel.xcdatamodel"
+      template "core_data.xcdatamodeld.erb", "gen/DataModel.xcdatamodeld/DataModel.xcdatamodel/contents", skip: true
+      
+      # Inject entity
+      inject_into_file "gen/DataModel.xcdatamodeld/DataModel.xcdatamodel/contents", before: "<elements>" do |config|
+        embed_template("entity.xml.erb", "    ")
+      end
+
+      # Add to elements list
+      inject_into_file "gen/DataModel.xcdatamodeld/DataModel.xcdatamodel/contents", before: "</elements>" do |config|
+        "<element name=\"#{model_name}\" positionX=\"0\" positionY=\"0\" width=\"0\" height=\"0\"/>\n"
+      end
+    end
+
     private
+
+    def core_data_type(ruby_type)
+      type = {
+        "integer" => "Integer 32",
+        "string" => "String",
+        "date" => "Date"
+      }[ruby_type.to_s]
+      raise "Don't know how to turn '#{ruby_type}' into a Core Data type" unless type
+      type
+    end
+
+    def embed_template(source, indent='')
+      template = File.join(self.class.source_root, source)
+      ERB.new(IO.read(template), nil, '-').result(binding).gsub(/^/, indent)
+    end
 
     def user_overwritten?
       ios_includes_file? "#{overwrite_filename}.h"
@@ -82,13 +113,17 @@ module RestkitGenerators
       name = association.name
 
       type = ''
-      if macro == :has_many or macro == :has_and_belongs_to_many
+      if macro_to_many? macro
         type = 'NSOrderedSet *'
       elsif macro == :belongs_to
         type = ios_class_name(name) + " *"
       end
       raise "Don't know how to turn association '#{macro}` '#{name}' into an Objective-C property" unless type
       type
+    end
+
+    def macro_to_many?(macro)
+      (macro == :has_many or macro == :has_and_belongs_to_many)
     end
 
   end
