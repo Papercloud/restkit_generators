3e26c2eb36adcb1b868e4f4ab7a66c647b120c77
Tomas Spacek <tomspacek@gmail.com>
2014-01-20 13:10:27 +1100

    Add install generator which creates the Generated.podspec file

---
+++ lib/generators/rest_kit/install/install_generator.rb
@@ -0,0 +1,27 @@
+module RestKit
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+    class_option :ios_path, type: :string, default: Rails.root
+
+    def generate_local_podspec
+      template "podspec.erb", destination_path("Generated.podspec")
+    end
+
+    def add_pod
+      append_to_file destination_path("Podfile") do |config|
+        "pod 'Generated', :path => './Generated.podspec'"
+      end
+    end
+
+    def generate_directory
+      empty_directory destination_path("Generated")
+    end
+
+    private
+
+    def destination_path(filename=nil)
+      File.join(File.expand_path(options[:ios_path]), filename)
+    end
+
+  end
+end
