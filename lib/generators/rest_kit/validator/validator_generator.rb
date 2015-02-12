4766a6bcba39d41f41ba691e9f539c873a2b4c14
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-02-12 14:28:13 +1100

    Added the `ValidatorGenerator` class, which, when run, will generate an extension / category that allows the core data object that the validator was generated for to be validated by running `[object validate]` / `object.validate()` depending on the language.
    These validators currently only support presence validation in Rails, but the fields are pulled from the Rails models which come with the validators, so support for new validations shouldn't be much of a drama.
    
    Added the swift template so far, which is just a copy of ones I've written already, doing the Obj-C version next.

---
+++ lib/generators/rest_kit/validator/validator_generator.rb
@@ -0,0 +1,39 @@
+module RestKit
+  class ValidatorGenerator < IosModelGenerator
+    source_root File.expand_path('../templates', __FILE__)
+
+    def generate_validator_extension
+      if use_swift
+        template "extension.swift.erb",   destination_path("#{filename}.swift")
+      else
+        template "interface.h.erb",       destination_path("#{filename}.h")
+        template "implementation.h.erb",  destination_path("#{filename}.m")
+      end
+    end
+
+    private
+
+    def destination_path(path)
+      super(File.join("Validators", path))
+    end
+
+    def filename
+      "#{ios_class_name(model_name)}+Validator"
+    end
+
+    def use_swift
+      options[:use_swift]
+    end
+
+    def validators
+      model.validators
+    end
+
+    def presence_validations
+      presence_validator = validators.find { |v| v.is_a? ActiveRecord::Validations::PresenceValidator }
+
+      presence_validator.try(:attributes).map(&:to_s) || []
+    end
+
+  end
+end
