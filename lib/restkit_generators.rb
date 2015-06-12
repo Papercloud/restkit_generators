c62b06a334c1615be472a8c048573ede7222db83
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-12 14:38:36 +1000

    Moved the `Config` class under the `lib/restkit_generators` module and added in some specs that test out the model configuration via the available options.

--- lib/restkit_generators.rb
+++ lib/restkit_generators.rb
@@ -1,6 +1,8 @@
 require 'rails/generators'
-require "restkit_generators/engine"
-require "restkit_generators/railtie"
+
+require 'restkit_generators/engine'
+require 'restkit_generators/railtie'
+require 'restkit_generators/config'
 
 module RestkitGenerators
 end
