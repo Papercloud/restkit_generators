58b990e5b7b23a3e3b02f859598fb503c408107a
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-16 11:10:20 +1000

    Some more basic specs around the mapping files being generated.

--- spec/lib/generators/mapping_generator_spec.rb
+++ spec/lib/generators/mapping_generator_spec.rb
@@ -14,12 +14,42 @@ describe RestKit::MappingGenerator do
     expect(File).to exist(File.join(destination_root, 'Mappings/MappingProtocol.h'))
   end
 
-  it 'creates an interface file' do
-    expect(File).to exist(File.join(destination_root, 'Mappings/_Post+Mapping.h'))
+  context 'interface file' do
+    let(:file) { File.join(destination_root, 'Mappings/_Post+Mapping.h') }
+
+    it 'creates an interface file' do
+      expect(File).to exist(file)
+    end
+
+    it 'specifies the correct class interface' do
+      expect(File.read(file)).to include '@interface _Post'
+    end
+
+    it 'specifies the correct category' do
+      expect(File.read(file)).to include '@interface _Post (Mapping)'
+    end
   end
 
-  it 'creates an implementation file' do
-    expect(File).to exist(File.join(destination_root, 'Mappings/_Post+Mapping.m'))
-  end
+  context 'implementation file' do
+    let(:file) { File.join(destination_root, 'Mappings/_Post+Mapping.m') }
 
+    it 'creates an implementation file' do
+      expect(File).to exist(file)
+    end
+
+    it 'imports the header file' do
+      expect(File.read(file)).to include '#import "_Post+Mapping.h"'
+    end
+
+    it 'imports the mapping files belong to included associations' do
+    end
+
+    it 'specifies the correct class implementation' do
+      expect(File.read(file)).to include '@implementation _Post'
+    end
+
+    it 'specifies the correct category' do
+      expect(File.read(file)).to include '@implementation _Post (Mapping)'
+    end
+  end
 end
