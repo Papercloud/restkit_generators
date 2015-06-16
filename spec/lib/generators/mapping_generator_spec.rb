548e3cac8408dc88e9ba296414b3e392677e812c
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-16 10:55:44 +1000

    New spec to check that the mapping protocol file is generated.

--- spec/lib/generators/mapping_generator_spec.rb
+++ spec/lib/generators/mapping_generator_spec.rb
@@ -10,12 +10,16 @@ describe RestKit::MappingGenerator do
     run_generator
   end
 
-  it "creates an interface file" do
-    expect(File).to exist(File.join(destination_root, "Mappings/_Post+Mapping.h"))
+  it 'creates a protocol header file' do
+    expect(File).to exist(File.join(destination_root, 'Mappings/MappingProtocol.h'))
   end
 
-  it "creates an implementation file" do
-    expect(File).to exist(File.join(destination_root, "Mappings/_Post+Mapping.m"))
+  it 'creates an interface file' do
+    expect(File).to exist(File.join(destination_root, 'Mappings/_Post+Mapping.h'))
+  end
+
+  it 'creates an implementation file' do
+    expect(File).to exist(File.join(destination_root, 'Mappings/_Post+Mapping.m'))
   end
 
 end
