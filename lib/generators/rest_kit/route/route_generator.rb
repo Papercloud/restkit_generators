9fac3f6af0cb1e35afd95964bb612f87f2a1913f
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-17 17:40:51 +1000

    Adds a `model` class option for the route generator, allowing a class name to be specified.
    In case the model can't be guessed by `model_name` based off the route, this allows for a model to be manually specified to be used during route generation.

--- lib/generators/rest_kit/route/route_generator.rb
+++ lib/generators/rest_kit/route/route_generator.rb
@@ -10,6 +10,9 @@ module RestKit
     class_option :serializer, type: :string, default: nil,\
      desc: "Name of the active_model_serializer from which infer RKResponseDescriptors"
 
+    class_option :model, type: :string, default: nil,\
+     desc: "Name of the active_model_serializer from which infer RKResponseDescriptors"
+
     def generate_route
       empty_directory destination_path("")
       template "interface.h.erb",       destination_path("#{filename}.h")
@@ -50,8 +53,10 @@ module RestKit
     end
 
     def model_name
-      model_name = route.name.split("_").last
+      model_name = options[:model].present? ? options[:model] : route.name.split("_").last
+
       raise "Can't infer serializer name from model name from '#{route.name}'" unless model_name
+
       model_name.singularize
     end
 
