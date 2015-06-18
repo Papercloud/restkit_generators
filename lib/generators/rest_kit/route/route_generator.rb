0e1642b89272cc36c0182a8bb15283272b177308
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-18 15:48:50 +1000

    Route generator rewrite to remove all of the direct calls to the generator to grab names, models, associations, etc. And now we use a single instance of our `ios/route` class to generate everything.

--- lib/generators/rest_kit/route/route_generator.rb
+++ lib/generators/rest_kit/route/route_generator.rb
@@ -20,7 +20,7 @@ module RestKit
     end
 
     def generate_response_descriptors
-      associations.each do |association|
+      route.associations.each do |association|
         inject_into_file destination_path("#{filename}.m"), after: "{\n" do |config|
           embed_template("response_descriptor.m.erb", "  ", binding())
         end
@@ -47,76 +47,19 @@ module RestKit
     private
 
     def route
-      route = Rails.application.routes.routes.named_routes[name]
-      raise "Could not find named route '#{name}'" unless route
-      route
-    end
-
-    def model_name
-      model_name = options[:model].present? ? options[:model] : route.name.split("_").last
-
-      raise "Can't infer serializer name from model name from '#{route.name}'" unless model_name
-
-      model_name.singularize
-    end
-
-    def model
-      @model ||= RestkitGenerators::Ios::Model.new(model_name, config)
+      @route ||= RestkitGenerators::Ios::Route.new(name, options, config)
     end
 
     def destination_path(path)
       super(File.join("Routes", path))
     end
 
-    def ios_route_name
-      strip = options[:strip_namespace]
-      if route.name[0...strip.length] == strip
-        strip = strip + "_" unless strip.include? "_"
-        return route.name[strip.length..route.name.length]
-      else
-        route.name
-      end
-    end
-
-    def ios_route_verb
-      route.verb.source.gsub(/[$^]/, '')
-    end
-
-    def ios_route_path
-      route.path.spec.map(&:left).select{ |p| p.is_a? String }.join.gsub(':format', 'json')
-    end
-
     def category_name
-      (ios_route_name + "_route").camelize
+      @category_name ||= (route.ios_route_name + "_route").camelize
     end
 
     def filename
-      "RKObjectManager+" + (ios_route_name + "_route").camelize
-    end
-
-    def serializer
-      if options[:serializer] and options[:serializer].camelize.include? "Serializer"
-        options[:serializer].constantize
-      elsif options[:serializer]
-        (options[:serializer] + "_serializer").camelize.constantize
-      else
-        (model_name + "_serializer").camelize.constantize
-      end
-    end
-
-    # Associations specified in the serializer, which we assume are embedded as unique objects under their own root.
-    def associations
-      serializer_associations = serializer._associations.keys
-      model.associations.select{ |a| serializer_associations.include?(a.name) }.map{ |a| RestkitGenerators::Ios::Association.new(a, config) }
-    end
-
-    # Root for the main object requested. Plural for index actions, singular for show and others.
-    def root_name
-      if route.defaults[:action] == "index"
-        model.model_name.demodulize.downcase.pluralize
-      else
-        model.model_name.demodulize.downcase.singularize
-      end
+      "RKObjectManager+" << category_name
     end
   end
 end
