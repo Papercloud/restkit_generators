57f9916a9f2548c512a3e695c140f89d082bd46a
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2015-06-30 17:30:00 +1000

    Moved the `config` related stuff out of the helpers class and into the base module of the gem, letting me grab it from anywhere pretty easily.
    Seeing as the config should be a gem-wide setup, makes a bit more sense to have it here than passing it around wherever.

--- lib/generators/rest_kit/helpers.rb
+++ lib/generators/rest_kit/helpers.rb
@@ -19,50 +19,6 @@ module RestKit
       }
     end
 
-    def config
-      @config ||= RestkitGenerators::Config.new(config_file_path)
-    end
-
-    # @return [Array<String>] All model class names in the Rails project
-    def all_model_class_names
-      @all_model_class_names ||= -> {
-        Rails.application.eager_load!
-
-        models = Dir["#{Rails.root}/app/models/**/*.rb"].map do |m|
-          mp = m.reverse.chomp("#{Rails.root}/app/models/".reverse).reverse
-          mp.chomp('.rb').camelize
-        end
-
-        ActiveRecord::Base.descendants.map(&:name) & models
-      }.call
-    end
-
-    # @return [Array<String>] Model class names that we want to exclude, according to the config file.
-    def excluded_model_class_names
-      config.excluded_models
-    end
-
-    # @return [Array<String>] Model class names that we want to include, implying exclusion of any others, according to the config file.
-    def included_model_class_names
-      config.exclusively_included_models
-    end
-
-    # @return [Array<String] Model class names that we want included in the SDK.
-    def model_class_names
-      @model_class_names ||= ((included_model_class_names.any?) ? included_model_class_names : all_model_class_names) - excluded_model_class_names
-    end
-
-    def named_routes
-      @named_routes ||= config.named_routes
-    end
-
-    # Path of the YAML config file used to persist settings for excluding classes between runs
-    # of this generator.
-    # @return [String] Absolute path to the config file.
-    def config_file_path
-      Rails.root.join('.ios_sdk_config.yml')
-    end
-
     def association_exists?(model, association)
       singular_symbol = association.active_record.name.demodulize.underscore.to_sym
       plural_symbol = association.active_record.name.demodulize.pluralize.underscore.to_sym
--- lib/restkit_generators.rb
+++ lib/restkit_generators.rb
@@ -9,4 +9,50 @@ require 'restkit_generators/ios/association'
 require 'restkit_generators/ios/route'
 
 module RestkitGenerators
+  def self.config
+    @config ||= RestkitGenerators::Config.new(config_file_path)
+  end
+
+  # @return [Array<String>] Model class names that we want to exclude, according to the config file.
+  def self.excluded_model_class_names
+    config.excluded_models
+  end
+
+  # @return [Array<String>] Model class names that we want to include, implying exclusion of any others, according to the config file.
+  def self.included_model_class_names
+    config.exclusively_included_models
+  end
+
+  # @return [Array<String] Model class names that we want included in the SDK.
+  def self.model_class_names
+    @model_class_names ||= ((included_model_class_names.any?) ? included_model_class_names : all_model_class_names) - excluded_model_class_names
+  end
+
+  # @return [Array<String] Named routes that we want included in the SDK.
+  def self.named_routes
+    @named_routes ||= config.named_routes
+  end
+
+  private
+
+  # Path of the YAML config file used to persist settings for excluding classes between runs
+  # of this generator.
+  # @return [String] Absolute path to the config file.
+  def self.config_file_path
+    Rails.root.join('.ios_sdk_config.yml')
+  end
+
+  # @return [Array<String>] All model class names in the Rails project
+  def self.all_model_class_names
+    @all_model_class_names ||= -> {
+      Rails.application.eager_load!
+
+      models = Dir["#{Rails.root}/app/models/**/*.rb"].map do |m|
+        mp = m.reverse.chomp("#{Rails.root}/app/models/".reverse).reverse
+        mp.chomp('.rb').camelize
+      end
+
+      ActiveRecord::Base.descendants.map(&:name) & models
+    }.call
+  end
 end
